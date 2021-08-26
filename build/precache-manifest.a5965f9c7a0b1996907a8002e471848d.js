f1d754498f4744860f97d50af5bfe988e04c09f7
jatink124 <kaushaljatin48@gmail.com>
2021-08-26 18:25:30 +0530

    build-deploy

---
+++ build/precache-manifest.a5965f9c7a0b1996907a8002e471848d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24740e49c46edfa25b05b3682631e9ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7b15c58cae4c697f889",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "66ed7cbefbe3b2b40d73",
+    "url": "/static/css/main.df528412.chunk.css"
+  },
+  {
+    "revision": "b7b15c58cae4c697f889",
+    "url": "/static/js/2.f24fb9ec.chunk.js"
+  },
+  {
+    "revision": "66ed7cbefbe3b2b40d73",
+    "url": "/static/js/main.68ba6117.chunk.js"
+  },
+  {
+    "revision": "c735e6ac134fcd422e97",
+    "url": "/static/js/runtime~main.bdf08e93.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
